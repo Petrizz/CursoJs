fef3e1bf3a8b8b742841aa29e95e39f372ed7914
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-03-06 11:56:21 -0300

    Finalizei
    agora falta ver o video e fazer as alterações que o guanabara fez para melhorar também meu exercicio

--- Exercicios/Idade/script.js
+++ Exercicios/Idade/script.js
@@ -4,35 +4,37 @@ function calcular(){
 var agora = new Date()
 var idade = document.querySelector('input#ano')
 var texto = document.querySelector('div#texto')
+var img = document.querySelector('div#img')
 var sexo = document.getElementsByName('sexo')
+
 
 idade = agora.getFullYear() - Number(idade.value) 
 
 
 
 if(sexo[0].checked){
-    texto.innerHTML = `Você é um <strong>homem</strong> com <strong>${idade}</strong> anos`
+    texto.innerHTML = `Você é <strong>masculino</strong> com <strong>${idade}</strong> anos`
     if(idade <= 10){
-        alert('criança')
+        img.innerHTML = '<img src="img/menino.png" alt="menino">'
     }else if(idade <= 21){
-        alert('adolecente')
+        img.innerHTML = '<img src="img/adolecenteM.png" alt="adolecenteMasculino">'
     }else if(idade <= 60){
-        alert('adulto')
+        img.innerHTML = '<img src="img/homem.png" alt="adulto">'
     }else if(idade > 60){
-       alert('idoso')
+       img.innerHTML ='<img src="img/velho.png" alt="velho">'
     }
     
 }
 if(sexo[1].checked){
-    texto.innerHTML = `Você é uma <strong>mulher</strong> com <strong>${idade}</strong> anos`
+    texto.innerHTML = `Você é <strong>feminina</strong> com <strong>${idade}</strong> anos`
     if(idade <= 10){
-        alert('criança')
+        img.innerHTML = '<img src="img/menina.png" alt="menina">'
     }else if(idade <= 21){
-        alert('adolecente')
+        img.innerHTML = '<img src="img/adolecenteF.png" alt="adolecenteFeminino">'
     }else if(idade <= 60){
-        alert('adulto')
+        img.innerHTML = '<img src="img/mulher.png" alt="adulta">'
     }else if(idade > 60){
-       alert('idoso')
+       img.innerHTML ='<img src="img/velha.png" alt="velha">'
     }
 }
 }
