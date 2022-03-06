ded1f9683ff6c74afcd04fb9c78187ae747502ea
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-03-06 11:42:10 -0300

    troca de img

--- Exercicios/horaDoDia/script.js
+++ Exercicios/horaDoDia/script.js
@@ -9,7 +9,7 @@ function carregar() {
    if(hora >= 0 && hora <= 6) {
 
         msg.innerHTML += '<p> Boa Madrugada!!</p>'
-        img.innerHTML = '<img src="img/boaMadrugada-240.jpg">'
+        img.innerHTML = '<img src="img/boaMadrugada-240.png">'
         window.document.body.style.background = 'black'
 
 
@@ -17,19 +17,19 @@ function carregar() {
     }else if( hora >= 6 && hora < 12){
 
         msg.innerHTML += '<p> Bom dia!!</p>'
-        img.innerHTML = '<img src="img/bomDia-240.jpg">'
+        img.innerHTML = '<img src="img/bomDia-240.png">'
         window.document.body.style.background = 'blue'
 
     }else if(hora >= 12 && hora <= 18){
 
         msg.innerHTML += '<p> Boa tarde!!</p>'
-        img.innerHTML = '<img src="img/boaTarde-240.jpg">'
+        img.innerHTML = '<img src="img/boaTarde-240.png">'
         windom.document.body.style.background = 'red'
 
     }else if( hora > 18){
 
         msg.innerHTML += '<p> Boa noite!!</p>'
-        img.innerHTML = '<img src="img/boaNoite-240.jpg">'
+        img.innerHTML = '<img src="img/boaNoite-240.png">'
         window.document.body.style.background = 'grey'
     }
 }
